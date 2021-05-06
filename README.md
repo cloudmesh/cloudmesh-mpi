21255804ad2671d789dc140a4c209cd058f68297
Gregor von Laszewski <laszewski@gmail.com>
2021-05-06 03:29:59 -0400

    add template

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# cloudmesh-mpi
+Documentation
+=============
+
+
+[![image](https://img.shields.io/travis/TankerHQ/cloudmesh-mpi.svg?branch=main)](https://travis-ci.org/TankerHQ/cloudmesn-mpi)
+
+[![image](https://img.shields.io/pypi/pyversions/cloudmesh-mpi.svg)](https://pypi.org/project/cloudmesh-mpi)
+
+[![image](https://img.shields.io/pypi/v/cloudmesh-mpi.svg)](https://pypi.org/project/cloudmesh-mpi/)
+
+[![image](https://img.shields.io/github/license/TankerHQ/python-cloudmesh-mpi.svg)](https://github.com/TankerHQ/python-cloudmesh-mpi/blob/main/LICENSE)
+
+see cloudmesh.cmd5
+
+* https://github.com/cloudmesh/cloudmesh.cmd5
+
+See for documentation 
---
+++ cloudmesh/mpi/command/mpi.py
@@ -0,0 +1,46 @@
+from cloudmesh.shell.command import command
+from cloudmesh.shell.command import PluginCommand
+from cloudmesh.mpi.api.manager import Manager
+from cloudmesh.common.console import Console
+from cloudmesh.common.util import path_expand
+from pprint import pprint
+from cloudmesh.common.debug import VERBOSE
+
+
+class MpiCommand(PluginCommand):
+
+    # noinspection PyUnusedLocal
+    @command
+    def do_mpi(self, args, arguments):
+        """
+        ::
+
+          Usage:
+                mpi --file=FILE
+                mpi list
+
+          This command does some useful things.
+
+          Arguments:
+              FILE   a file name
+
+          Options:
+              -f      specify the file
+
+        """
+        arguments.FILE = arguments['--file'] or None
+
+        VERBOSE(arguments)
+
+        m = Manager()
+
+        if arguments.FILE:
+            print("option a")
+            m.list(path_expand(arguments.FILE))
+
+        elif arguments.list:
+            print("option b")
+            m.list("just calling list without parameter")
+
+        Console.error("This is just a sample")
+        return ""
