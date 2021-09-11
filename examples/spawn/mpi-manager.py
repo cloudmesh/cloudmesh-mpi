78dd549b65126ca4ff9189aadd4c2dffee629efc
Gregor von Laszewski <laszewski@gmail.com>
2021-09-11 09:27:06 -0400

    test

--- examples/spawn/mpi-manager.py
+++ examples/spawn/mpi-manager.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 from mpi4py import MPI
+import mpi4py
 import numpy
 import sys
 import time
+from pprint import pprint
 print("Hello")
+
+print ("argv", sys.argv)
+
+#print ("ENV", dict(mpi4py.MPI.INFO_ENV))
+import os
+
+#pprint (dict(os.environ))
+
 comm = MPI.COMM_SELF.Spawn(sys.executable,
                            args=['mpi-worker.py'],
                            maxprocs=2)
