531abdeba7edd6026e204da4f2fab99de426965b
J.P <stapmoshun@gmail.com>
2021-09-25 00:05:23 -0400

    rename manager/worker for spawn and remove old prgs

--- examples/spawn/mpi-manager.py
+++ examples/spawn/mpi-manager.py
@@ -1,38 +1,18 @@
 #!/usr/bin/env python
 from mpi4py import MPI
-import mpi4py
 import numpy
 import sys
-import time
-from pprint import pprint
-from cloudmesh.common.util import banner
+import psutil
 
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-#name = comm.Get_processor_name()
-name = mpi4py.MPI.Get_processor_name()
-
-banner(f"MPI Spawn example {name} {size}")
-
-icomm = MPI.COMM_SELF.Spawn(
-    sys.executable,
-    args=['mpi-worker.py'],
-    maxprocs=size)
-
-rank = icomm.Get_rank()
-icomm.Bcast([size, MPI.INT])
-print(f"manager: rank of {name}: {rank} of {size}")
+comm = MPI.COMM_SELF.Spawn(sys.executable,
+                           args=['mpi-worker.py'],
+                           maxprocs=(psutil.cpu_count(logical=False) - 1))
 
 N = numpy.array(100, 'i')
-icomm.Bcast([N, MPI.INT], root=MPI.ROOT)
-#print(f"ROOT: {MPI.ROOT}")
-print('manager: c')
+comm.Bcast([N, MPI.INT], root=MPI.ROOT)
 PI = numpy.array(0.0, 'd')
-
-print('manager: d')
-icomm.Reduce(None, [PI, MPI.DOUBLE],
+comm.Reduce(None, [PI, MPI.DOUBLE],
             op=MPI.SUM, root=MPI.ROOT)
-print("manager:",PI)
+print(PI)
 
-time.sleep(30)
-icomm.Disconnect()
+comm.Disconnect()
