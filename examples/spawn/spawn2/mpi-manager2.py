73503b17f6153706f682da06cffb1cbab3c5a1c4
Fidel Leal <fidelxleal@gmail.com>
2021-09-24 23:00:58 -0500

    set maxprocs to cpu_count - 1

--- examples/spawn/spawn2/mpi-manager2.py
+++ examples/spawn/spawn2/mpi-manager2.py
@@ -2,10 +2,11 @@
 from mpi4py import MPI
 import numpy
 import sys
+import psutil
 
 comm = MPI.COMM_SELF.Spawn(sys.executable,
                            args=['mpi-worker2.py'],
-                           maxprocs=5)
+                           maxprocs=(psutil.cpu_count(logical=False) - 1))
 
 N = numpy.array(100, 'i')
 comm.Bcast([N, MPI.INT], root=MPI.ROOT)
