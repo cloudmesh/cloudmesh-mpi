96c68f0b74ca3232fedc8f793a0f58b859301eaf
J.P <70083705+stapmoshun@users.noreply.github.com>
2021-10-29 00:09:22 -0400

    add shell

---
+++ doc/chapters/slurm/slurm.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from cloudmesh.common.StopWatch import StopWatch
+from cloudmesh.common.util import banner
+from cloudmesh.common.Parameter import Parameter
+from cloudmesh.common.util import readfile,writefile
+import os
+
+#a = readfile("test1")
+#writefile("test2",a)
+
+StopWatch.start("Total Runtime")
+hosts = "red,red0[1-3]"
+ehosts = Parameter.expand(hosts)
+
+cloudmesh.banner("Slurm on Raspberry Pi Cluster Installation")
+
+#def parallel_execute(hosts,command):
+#  os.system("cms host ssh "+hosts+" \"'"+command+""'\"")
+#
+#parallel_execute(hosts,"sudo apt-get update")
+results = Host.ssh(hosts=hosts,command="sudo apt-get update")
+print(results)
+#parallel_execute(hosts,"sudo apt install ntpdate -y")
+results2 = Host.ssh(hosts=hosts,command="sudo apt install ntpdate -y")
+print(results2)
+
+#for host in ehosts:
+#  os.system("cms host ssh "+hosts+" 'touch step1'")
+
+results3 = Host.ssh(hosts=hosts,command="touch step1")
+print(results3)
+# print (names)
+
+results4 = Host.ssh(hosts=hosts,
+                   command="cat step1")
+print(results4)
+
+
+StopWatch.stop("Total Runtime")
+StopWatch.benchmark()
