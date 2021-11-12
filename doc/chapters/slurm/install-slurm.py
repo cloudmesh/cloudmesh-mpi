a8bf586246d22e06c5f45dca4c30fbde94bcd63a
J.P <stapmoshun@gmail.com>
2021-11-12 14:49:46 -0500

    finish slurm script

--- doc/chapters/slurm/install-slurm.py
+++ doc/chapters/slurm/install-slurm.py
@@ -99,7 +99,7 @@ def step2():
     card = SDCard()
     card.info()
     USB.check_for_readers()
-    print('Please enter the device path e.g. "/dev/sda" or no input default to /dev/sda:')
+    print('Please enter the device path e.g. "/dev/sda" or no input default to /dev/sda (remember, do not add quotation marks):')
     device = input()
     if device == '':
         device = '/dev/sda'
