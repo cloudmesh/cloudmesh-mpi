92430415de7a9debf46ab23f32dc3c27553118d1
hy <haiyang@gmail.com>
2022-01-03 22:12:54 -0500

    now includes order for generate_shuffle

--- examples/sort/generate.py
+++ examples/sort/generate.py
@@ -16,9 +16,18 @@ def generate_random(n):
     return a
 
 def generate_shuffle(order, n, m):
-    if order == "ascending":
+    if order in ["ascending","<"]:
         a = []
         for i in range(n):
+            a.append(i)
+        for i in range(m):
+            x1 = random.randint(0, n)
+            x2 = random.randint(0, n)
+            a[x1], a[x2] = a[x2], a[x1]
+        return a
+    elif order in ["descending",">"]: 
+        a = []
+        for i in range(n, 0):
             a.append(i)
         for i in range(m):
             x1 = random.randint(0, n)
