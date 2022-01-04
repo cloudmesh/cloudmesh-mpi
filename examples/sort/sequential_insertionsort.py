ca095914e282eece42219af061190dd970085a0e
hy <haiyang@gmail.com>
2022-01-03 22:11:42 -0500

    updated

--- examples/sort/sequential_insertionsort.py
+++ examples/sort/sequential_insertionsort.py
@@ -1,23 +1,40 @@
+from verify import verify
+from generate import generate_random
+from generate import generate_shuffle
+from cloudmesh.common.StopWatch import StopWatch
+from performance import assess
 #insertion sort
-import sys;
 
-def insertion_sort(arr, n):
-    for i in range(1, n):
-        x = arr[i]
-        j = i - 1
-        #shift all elements greater than x to the right by 1
-        while j >= 0 and x < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        #insert x
-        arr[j + 1] = x
+def insertion_sort(order, arr):
+    n = len(arr)
+    if order in ["ascending","<"]: 
+        for i in range(1, n):
+            x = arr[i]
+            j = i - 1
+            #shift all elements greater than x to the right by 1
+            while j >= 0 and x < arr[j]:
+                arr[j + 1] = arr[j]
+                j -= 1
+            #insert x
+            arr[j + 1] = x
+    elif order in ["descending",">"]: 
+        for i in range(1, n):
+            x = arr[i]
+            j = i - 1
+            #shift all elements less than x to the right by 1
+            while j >= 0 and x > arr[j]:
+                arr[j + 1] = arr[j] 
+                j -= 1
+            #insert x
+            arr[j + 1] = x
 
+
+#test and input
 if __name__ == "__main__":
-    #test and input
     a = []
-    n = int(input())
-    for i in range(n):
-        x = int(input())
-        a.append(x)
-    insertion_sort(a, len(a))
+    a = generate_random(20)
+    insertion_sort(">", a)
+    print(verify("ascending", a))
     print(a)
+
+    assess(insertion_sort, "insertion_sort")
