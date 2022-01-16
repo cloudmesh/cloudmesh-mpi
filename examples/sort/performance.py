baff052e4a306cd4d58b3b81dfb5cd8ec53a2b20
hy <haiyang@gmail.com>
2022-01-16 18:12:18 -0500

    added multiprocessing assess

--- examples/sort/performance.py
+++ examples/sort/performance.py
@@ -3,10 +3,23 @@ from cloudmesh.common.StopWatch import StopWatch
 from verify import verify
 
 def assess(sort_type, sort_name):
-    for n in [10, 100, 1000]:
-            a = generate_random(n)
-            StopWatch.start(f"{sort_name}_{n}")
-            sort_type("<", a)
-            StopWatch.stop(f"{sort_name}_{n}")
-            assert verify("ascending", a)
-    StopWatch.benchmark()
+	for n in [10, 100, 1000]:
+		a = generate_random(n)
+		print(a)
+		StopWatch.start(f"{sort_name}_{n}")
+		sort_type("<", a)
+		StopWatch.stop(f"{sort_name}_{n}")
+		print(a)
+		assert verify("ascending", a)
+	StopWatch.benchmark()
+
+def multiprocessing_assess(sort_type, sort_name, processes):
+	for n in [10, 100, 1000]:
+		a = generate_random(n)
+		print(a)
+		StopWatch.start(f"{sort_name}_{n}")
+		a = sort_type(a, processes) 
+		StopWatch.stop(f"{sort_name}_{n}")
+		print(a)
+		assert verify("ascending", a)
+	StopWatch.benchmark()
