6d4537a162b5856e321f8c6d1fb84e6efb438c19
hy <haiyang@gmail.com>
2022-01-16 22:44:02 -0500

    working with run.sh

--- examples/sort/performance.py
+++ examples/sort/performance.py
@@ -2,24 +2,30 @@ from generate import generate_random
 from cloudmesh.common.StopWatch import StopWatch
 from verify import verify
 
+debug = False
+
 def assess(sort_type, sort_name):
 	for n in [10, 100, 1000]:
 		a = generate_random(n)
-		print(a)
-		StopWatch.start(f"{sort_name}_{n}")
+		if debug:
+			print(a)
+		StopWatch.start(f"{sort_name}_{n}_1")
 		sort_type("<", a)
-		StopWatch.stop(f"{sort_name}_{n}")
-		print(a)
+		StopWatch.stop(f"{sort_name}_{n}_1")
+		if debug:
+			print(a)
 		assert verify("ascending", a)
 	StopWatch.benchmark()
 
-def multiprocessing_assess(sort_type, sort_name, processes):
-	for n in [10, 100, 1000]:
+def multiprocessing_assess(sort_type, sort_name, processes, sizes):
+	for n in sizes:
 		a = generate_random(n)
-		print(a)
-		StopWatch.start(f"{sort_name}_{n}")
+		if debug: 
+			print(a)
+		StopWatch.start(f"{sort_name}_{n}_{processes}")
 		a = sort_type(a, processes) 
-		StopWatch.stop(f"{sort_name}_{n}")
-		print(a)
+		StopWatch.stop(f"{sort_name}_{n}_{processes}")
+		if debug:
+			print(a)
 		assert verify("ascending", a)
 	StopWatch.benchmark()
