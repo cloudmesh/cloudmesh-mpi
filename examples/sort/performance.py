8ee359412544bbb524aae6657796087c890c1cd9
hy <haiyang@gmail.com>
2022-01-22 17:52:43 -0500

    removed count

--- examples/sort/performance.py
+++ examples/sort/performance.py
@@ -17,15 +17,14 @@ def assess(sort_type, sort_name):
 		assert verify("ascending", a)
 	StopWatch.benchmark()
 
-def multiprocessing_assess(sort_type, sort_name, processes, sizes):
-	for n in sizes:
-		a = generate_random(n)
-		if debug: 
-			print(a)
-		StopWatch.start(f"{sort_name}_{n}_{processes}")
-		a = sort_type(a, processes) 
-		StopWatch.stop(f"{sort_name}_{n}_{processes}")
-		if debug:
-			print(a)
-		assert verify("ascending", a)
+def multiprocessing_benchmark(sort_type, sort_name, processes, size):
+	a = generate_random(size)
+	if debug: 
+		print(a)
+	StopWatch.start(f"{sort_name}_{size}_{processes}")
+	a = sort_type(a, processes) 
+	StopWatch.stop(f"{sort_name}_{size}_{processes}")
+	if debug:
+		print(a)
+	assert verify("ascending", a)
 	StopWatch.benchmark()
