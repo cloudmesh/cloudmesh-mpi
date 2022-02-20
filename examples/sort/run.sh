5a83fc2404fbe4990cca377babfa2e41df92cc2b
hy <haiyang@gmail.com>
2022-02-20 17:59:04 -0500

    updating processes

--- examples/sort/run.sh
+++ examples/sort/run.sh
@@ -9,7 +9,7 @@
 pip install -r requirements.txt
 
 #./experiment.py --processes="[1-p]" --size="[100]" --repeat=10 | fgrep "# csv" | tee output.log
-./experiment.py --processes="[1-t]" --size="[100]" --repeat=10 | tee output.log
+./experiment.py --processes="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]" --size="[100]" --repeat=10 | tee output.log
 #| fgrep "# csv" | tee output.log
 
 #python graph_efficiency.py
