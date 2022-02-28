06db0cb591532a102b3e0197e5ec71f1b9e13b22
Gregor von Laszewski <laszewski@gmail.com>
2022-02-28 09:19:24 -0500

    remove too much code

--- examples/sort/analysis.py
+++ examples/sort/analysis.py
@@ -71,215 +71,12 @@ def read_logs(files=["alex", "gregor"], size =[100], tags=["multiprocessing_merg
 
     return data
 
-
-
-# NOT USED in notebook
-def generate_average(df, tag, size):
-    pass
-
-
-
-# NOT USED in notebook
-def avg_simple_processes_time_fixed_size(data1, size, name=None, processes=None, label=None):
-    """
-    creates image from averaged data with processes and time while keeping size constant
-
-    :param data1:
-    :type data1:
-    :param size:
-    :type size:
-    :param name:
-    :type name:
-    :param processes:
-    :type processes:
-    :param label:
-    :type label:
-    :return:
-    :rtype:
-    """
-
-    x = []
-    y = []
-
-    for entry in data1:
-        p, s, count, t = entry
-        if processes is None or p in processes and s == size:
-            x.append(p)
-            y.append(t)
-    return x, y
-
-# NOT USED in notebook
-def format_plot():
-    sns.set_theme(style="darkgrid")
-    df1 = pd.DataFrame({'processes': x1, 'time': y1})
-    avg1 = df1.groupby(['processes']).mean()
-    sns.lineplot(x="processes", y="time", data=avg1)
-    plt.show()
-
-# NOT USED in notebook
-def get_ranges(data):
-    """
-    TBD
-
-    :param data:
-    :type data:
-    :return:
-    :rtype:
-    """
-    processes = []
-    sizes = []
-    counts = []
-    for line in data:
-        name, p, s, c, time, total = line
-        processes.append(p)
-        sizes.append(s)
-        counts.append(c)
-    return {
-        "processes": list(set(processes)),
-        "sizes": list(set(sizes)),
-        "counts": len(list(set(counts)))
-    }
-
-# NOT USED in notebook
-def processes_time_fixed_size(data, size, name=None, processes=None, label=None):
-    """
-    creates image from data with processes and time while keeping size constant
-
-    :param data:
-    :type data:
-    :param size:
-    :type size:
-    :param name:
-    :type name:
-    :param processes:
-    :type processes:
-    :param label:
-    :type label:
-    :return:
-    :rtype:
-    """
-    x = []
-    y = []
-    result = []
-
-    for entry in data:
-        label, p, s, count, t, total, host, user = entry
-        if processes is None or p in processes and s == size:
-            x.append(p)
-            y.append(t)
-
-    result = {
-        "x": x,
-        "y": y
-    }
-
-    pprint(x)
-    pprint(y)
-
-    if name == None:
-        name = f"images/processes_time_{size}"
-    sns.set_theme(style="ticks", palette="pastel")
-    ax = sns.boxplot(x="x", y="y", data=result)
-    label = label.replace("_", " ")
-    ax.set_title(f"{label}, size={size}")
-    ax.set_ylabel("Time/s")
-    ax.set_xlabel("Processes")
-    # plt.savefig(f"{name}.png")
-    # plt.savefig(f"{name}.pdf")
-    plt.show()
-    plt.close()
-
-# NOT USED in notebook
-def avg_processes_time_fixed_size(data1, size, name=None, processes=None, label=None):
-    """
-    creates image from averaged data with processes and time while keeping size constant
-
-    :param data1:
-    :type data1:
-    :param size:
-    :type size:
-    :param name:
-    :type name:
-    :param processes:
-    :type processes:
-    :param label:
-    :type label:
-    :return:
-    :rtype:
-    """
-
-    x1 = []
-    y1 = []
-
-    for entry in data1:
-        label, p, s, count, t, total, host, user = entry
-        if processes is None or p in processes and s == size:
-            x1.append(p)
-            y1.append(t)
-
-    sns.set_theme(style="darkgrid")
-    df1 = pd.DataFrame({'processes': x1, 'time': y1})
-    avg1 = df1.groupby(['processes']).mean()
-    ax = sns.lineplot(x="processes", y="time", data=avg1)
-    ax.set_title(f"{label}, size={size}")
-    ax.set_ylabel("Time/s")
-    ax.set_xlabel("Processes")
-    plt.show()
-
-# NOT USED in notebook
-def speedup_fixed_size(data, size, name, processes):
-    """
-    creates image from data to show speedup with fixed size
-
-    :param data:
-    :type data:
-    :param size:
-    :type size:
-    :param name:
-    :type name:
-    :param processes:
-    :type processes:
-    :return:
-    :rtype:
-    """
-    x = []
-    y = []
-    result = []
-    nums = []
-    pprint(processes)
-    for p in processes:
-        print(p)
-        temp = []
-        for entry in data:
-            label, p, s, count, t, total, host, user = entry
-            if size == s:
-                print("HERE")
-                print(t)
-                temp.append(t)
-        nums.append(temp)
-    pprint(nums)
-    time_0 = sum(nums[0][i] for i in len(nums[0])) / len(nums[0])
-    for i in len(nums):
-        time = sum(nums[i][j] for j in len(nums[i])) / len(nums[i])
-        x.append(processes[i])
-        y.append(float(time_0 / time))
-        result.append([processes[i], float(time_0 / time)])
-
-    if name == None:
-        name = f"speedup_{size}"
-    sns.set_theme(style="ticks", palette="pastel")
-    ax = sns.boxplot(x="x", y="y", data=result)
-    label = label.replace("_", " ")
-    ax.set_title(f"{label}, size={size}")
-    ax.set_ylabel("speedup")
-    ax.set_xlabel("processes")
-    # plt.savefig(f"{name}.png")
-    # plt.savefig(f"{name}.pdf")
-    plt.show()
-    plt.close()
-
-
-# plt.savefig('images/size.pdf')
+def generate_average(df, tag=None, size=None, name=None):
+    _df = df.loc[(df['name'] == name) & (df['tag'] == tag) &  (df['size'] == size) ]
+    avg = _df.groupby(['processors', 'name', 'size', 'tag']).mean()
+    avg['tag'] = tag
+    avg['name'] = name
+    return avg
 
 @click.command()
 @click.option('--processes', default="[1]", help='Number of processes as array. [1-2,8,16]')
@@ -336,14 +133,10 @@ def analysis(processes, size, repeat, log, debug, sort, x, y, info):
     f = open(log, "r")
     content = f.read().splitlines()
     data = get_data(content, name=sort)
-    for size in sizes:
-        avg_processes_time_fixed_size(data, size, label=sort)
     if debug:
         print(content)
         pprint(data)
 
-    if info:
-        pprint(get_ranges(data))
     return
 
     try:
