305b77f9b4295eeb5452ff80221628a00e42a6fc
hy <haiyang@gmail.com>
2022-05-28 11:50:21 -0400

    mpi merge

--- examples/sort/mpi_mergesort.py
+++ examples/sort/mpi_mergesort.py
@@ -12,3 +12,43 @@ final_sorted = np.zeros(N, dtype="int")
 local_array = np.zeros(int(N / size), dtype="int")
 local_tmp = np.zeros(int(N / size), dtype="int")
 local_remain = np.zeros(2 * int(N / size), dtype="int")
+
+if rank == 0:
+    unsorted = np.random.randint(low=0,high=N,size=N)
+    print (unsorted)
+comm.Scatter(unsorted, local_array, root = 0)
+
+step = size / 2
+print ("Rank: ", rank)
+while (step >= 1):
+    if (rank >= step and rank < step * 2):
+        comm.Send(local_array, rank - step, tag = 0)
+    elif (rank < step):
+        local_tmp = np.zeros(local_array.size, dtype="int")
+        local_remain = np.zeros(2 * local_array.size, dtype="int")
+        comm.Recv(local_tmp, rank + step, tag = 0)
+        i = 0 
+        j = 0 
+        for k in range (0, 2 * local_array.size):
+            if (i >= local_array.size):
+                local_remain[k] = local_tmp[j]
+                j += 1
+            elif (j >= local_array.size):
+                local_remain[k] = local_array[i]
+                i += 1
+            elif (local_array[i] > local_tmp[j]):
+                local_remain[k] = local_tmp[j]
+                j += 1
+            else:
+                local_remain[k] = local_array[i]
+                i += 1        
+        print ("Step: ", step)
+        print ("local array: ", local_array)
+        print ("local tmp: ", local_tmp)
+        print ("local remain: ", local_remain)
+        local_array = local_remain
+    step = step / 2
+    
+if (rank  == 0):
+    print (local_array)
+
