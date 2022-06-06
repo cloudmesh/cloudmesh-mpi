62e2a79849434624244ba99abb3bb935c2fd3967
J.P <stapmoshun@gmail.com>
2022-06-06 16:28:51 -0400

    100 jobs

--- examples/slurm/100jobs.py
+++ examples/slurm/100jobs.py
@@ -7,6 +7,7 @@ describe what this is
 from cloudmesh.common.Shell import Shell
 from cloudmesh.common.util import writefile
 from cloudmesh.common.StopWatch import StopWatch
+import random
 
 n = number_of_jobs = 5
 maximum_time=10
