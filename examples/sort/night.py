20ee18f76e9f962a3d9240073cbd095863dff66d
hy <haiyang@gmail.com>
2022-06-14 02:23:36 -0400

    mpi merge sort

--- examples/sort/night.py
+++ examples/sort/night.py
@@ -1,13 +1,12 @@
 # run this program using mpiexec -n 4 python mpi_mergesort.py
 
-# http://selkie-macalester.org/csinparallel/modules/MPIProgramming/build/html/mergeSort/mergeSort.html#parallel-algorithm
-
 import numpy as np
 from mpi4py import MPI
 import random
 import numpy as np
 from generate import Generator
 from cloudmesh.common.StopWatch import StopWatch
+from cloudmesh.common.dotdict import dotdict
 import math
 
 config = dotdict()
@@ -79,40 +78,64 @@ elif config.algorithm=="sequential_merge_fast":
 
 
 if __name__ == '__main__':
-    global_arr = np.zeros(n, dtype="int")
+    unsorted_arr = np.zeros(n, dtype="int")
+    sorted_arr = np.zeros(n, dtype="int")
+
     sub_size = int(n / size)
+    local_arr = np.zeros(sub_size, dtype="int")
+    local_tmp = np.zeros(sub_size, dtype="int")
+    local_remain = np.zeros(2 * sub_size, dtype="int")
 
     if rank == 0:
         StopWatch.start(f"total-{rank}")
-        global_arr = np.array(Generator().generate_random(n))
-        print(f"UNSORTED ARRAY: {global_arr}")
-
-        global_arr = np.reshape(global_arr, (-1, sub_size))
-        # print(global_arr)
+        unsorted_arr = np.array(Generator().generate_random(n))
+        print(f"UNSORTED ARRAY: {unsorted_arr}")
 
 
     # Send subarray to each process
-    sub_arr = np.zeros(sub_size, dtype="int")
-    comm.Scatter(global_arr, sub_arr, root=0)
+    comm.Scatter(unsorted_arr, local_arr, root=0)
 
     # print(sub_size)
-    print(f'Buffer in process {rank} contains: {sub_arr}')
+    # print(f'Buffer in process {rank} contains: {sub_arr}')
 
     # Sort each subarray
-    sub_arr = sorted(sub_arr)
+    local_arr.sort()
 
-    print(f'Buffer in process {rank} before gathering: {sub_arr}')
+    # print(f'Buffer in process {rank} before gathering: {sub_arr}')
     # Gather sorted subarrays into one
-    sorted = None
-    if rank == 0:
-        sorted = np.zeros([size, sub_size], dtype="int")
-        print(f'recvbuf in process 0 before gathering: {sorted}')
-     
-     
-    comm.Gather(sub_arr, sorted, root=0)
+    
+    step = size / 2
+    print(f"Rank: {rank}")
+    while step >= 1:
+        if rank >= step and rank < step * 2:
+            comm.Send(local_arr, rank - step, tag=0)
+        elif rank < step:
+            local_tmp = np.zeros(local_arr.size, dtype="int")
+            local_remain = np.zeros(2 * local_arr.size, dtype="int")
+            comm.Recv(local_tmp, rank + step, tag=0)
+
+            i = 0
+            j = 0
+            for k in range(0, 2 * local_arr.size):
+                if i >= local_arr.size:
+                    local_remain[k] = local_tmp[j]
+                    j += 1
+                elif j >= local_arr.size:
+                    local_remain[k] = local_arr[i]
+                    i += 1
+                elif local_arr[i] > local_tmp[j]:
+                    local_remain[k] = local_tmp[j]
+                    j += 1
+                else:
+                    local_remain[k] = local_arr[i]
+                    i += 1
+            
+            local_arr = local_remain
+        step = step / 2
+
     if rank == 0:
         StopWatch.stop(f"total-{rank}")
-        print(f'recvbuf in process 0 after gathering: \n{sorted}')
+        print(f"SORTED ARRAY: {local_arr}")
     
-StopWatch.benchmark(user=config.user, host=config.host)
-StopWatch.benchmark(config.logfile, user=config.user, host=config.host)
+StopWatch.benchmark(user=config.user)
+StopWatch.benchmark(config.logfile, user=config.user)
