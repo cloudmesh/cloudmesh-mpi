ac8355fc6e7833870e13e386d82c2d8e0f62bcb4
hy <haiyang@gmail.com>
2022-09-17 19:26:11 -0400

    results

--- examples/sort/results.py
+++ examples/sort/results.py
@@ -9,20 +9,20 @@ from analysis import read_log, read_logs
 from analysis import generate_average
 
 user = "alex"
-node = "2400MHz"
+node = "v100"
 sorts = ["seq-merge", "mp-mergesort"]
 sort = "mp-mergesort"
 
-sizes = [100, 1000, 10e4, 10e5]
+sizes = [100, 1000, 10e3, 10e4, 10e5, 10e6, 5*10e6, 10e7, 5*10e7, 10e8]
 sizes = [int(size) for size in sizes]
 
 # this will take a long time. 
 # also if you don't want to use all your processors then use different commands. 
-# for sort in sorts:
-    # for size in sizes:
-        # run_cmd = f"./run.py --user={user} --node={node} --size={size} --sort={sort}"
-        # banner(run_cmd)
-        # os.system(run_cmd)
+for sort in sorts:
+    for size in sizes:
+        run_cmd = f"./run.py --user={user} --node={node} --size={size} --sort={sort}"
+        banner(run_cmd)
+        os.system(run_cmd)
 
 
 files = ["alex"]
