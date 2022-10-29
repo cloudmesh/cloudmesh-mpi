709c4a5aeaeaf162246630db1d45d45eb3e5fa07
hy <haiyang@gmail.com>
2022-10-29 13:59:56 -0400

    testing analysis

---
+++ examples/sort/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from pprint import pprint
+
+import matplotlib.pyplot as plt
+import json
+
+from cloudmesh.common.Shell import Shell
+from cloudmesh.common.parameter import Parameter
+from analysis import read_log
+
+log = "mp-v100-alex-100-10-None-None.log"
+print(read_log(log))
